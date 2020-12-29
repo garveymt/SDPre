013005fb2c07b845129abda778a2bc90819e8685
garveymt <75683436+garveymt@users.noreply.github.com>
2020-12-29 14:55:55 -0700

    Add files via upload

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int sumTotal = 0;
+    int counter = 1;
+    while (counter <= 20) {
+      sumTotal += counter;
+      counter ++;
+    }
+    System.out.println(sumTotal);
+  }
+}
---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,9 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int currentSumTotal = 0;
+    for (int i = 1; i <=20; i++) {
+      currentSumTotal += i;
+    }
+      System.out.println(currentSumTotal);
+  }
+}
